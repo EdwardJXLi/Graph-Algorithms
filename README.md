54b9ead3c47f2eafdd9413ff4295103a667b46e7
Edward Li <radioactivehydra@gmail.com>
2020-04-25 16:07:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Graph Algorithms
+A collection of some graph algorithms I wrote during my free time  
+They contain pretty in-deaph comments, so feel free to use them  
+Some of these were homework for my [ALGO2](https://github.com/Team-ALGO2/ALGO2) Class  
+
+## Algorithms:
+- Advanced Pathfinding Algorithms
+  - Dijkstra’s algorithm (Default) (Working-ish)
+  - Bellman-Ford’s algorithm (Working-ish)
+
+- All Pairs Shortest Path Algorithms
+  - Floyd-Warshall’s algorithm (Default) (Working-ish)
+
+- Advanced Minimum Spanning Tree Algorithms
+  - Prim’s algorithm (Default) (Working-ish)
+  - Kruskal’s algorithm (**TODO**)
+  
+- Maximum Flow Algorithms
+  - Ford-Fulkerson algorithm (Default) (**TODO**)
+  - Dinic's algorithm (**TODO**)
+
+> Note: Some of these were made before learning `malloc`, rewrite incoming!
---
+++ kruskal.c
@@ -0,0 +1,64 @@
+//Kruskals Alternative Implementation
+
+#include <stdio.h>
+#define INFINITY 2147483647
+#define NEG_INFINITY -2147483647
+#define true 1
+#define false 0
+
+//Define Maximum Sizes
+#define KS_ALT_MAX_SIZE 9
+
+//The meat of the the algorithm
+//Takes In:
+//map -> 2d matrix of distances between nodes
+//cMap -> 2d matrix of Connectivity Booleans
+//start -> Starting Node
+int kruskals(int map[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE], int cMap[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE], int start){
+    //TODO
+}
+
+
+int main(){
+	/*
+	int map[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE] = {
+	{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
+	{4, 0, 8, 0, 0, 0, 0, 11, 0}, 
+	{0, 8, 0, 7, 0, 4, 0, 0, 2}, 
+	{0, 0, 7, 0, 9, 14, 0, 0, 0}, 
+	{0, 0, 0, 9, 0, 10, 0, 0, 0}, 
+	{0, 0, 4, 14, 10, 0, 2, 0, 0}, 
+	{0, 0, 0, 0, 0, 2, 0, 1, 6}, 
+	{8, 11, 0, 0, 0, 0, 1, 0, 7}, 
+	{0, 0, 2, 0, 0, 0, 6, 7, 0}};
+
+	int cMap[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE] = {
+	{0, 1, 0, 0, 0, 0, 0, 1, 0}, 
+	{1, 0, 1, 0, 0, 0, 0, 1, 0}, 
+	{0, 1, 0, 1, 0, 1, 0, 0, 1}, 
+	{0, 0, 1, 0, 1, 1, 0, 0, 0}, 
+	{0, 0, 0, 1, 0, 1, 0, 0, 0}, 
+	{0, 0, 1, 1, 1, 0, 1, 0, 0}, 
+	{0, 0, 0, 0, 0, 1, 0, 1, 1}, 
+	{1, 1, 0, 0, 0, 0, 1, 0, 1}, 
+	{0, 0, 1, 0, 0, 0, 1, 1, 0}};
+	*/
+
+	int map[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE] = {
+		{0, 2, 0, 6, 0}, 
+        {2, 0, 3, 8, 5}, 
+        {0, 3, 0, 0, 7}, 
+        {6, 8, 0, 0, 9}, 
+        {0, 5, 7, 9, 0}
+    };
+
+	int cMap[KS_ALT_MAX_SIZE][KS_ALT_MAX_SIZE] = {
+		{0, 1, 0, 1, 0}, 
+        {1, 0, 1, 1, 1}, 
+        {0, 1, 0, 0, 1}, 
+        {1, 1, 0, 0, 1}, 
+        {0, 1, 1, 1, 0}
+    };
+
+	kruskals(map, cMap, 0);
+}
