253f70dc95570586a36e9379aa858248151ee281
Edward Li <radioactivehydra@gmail.com>
2020-04-25 19:55:22 -0400

    Updated Dijkstras To Use Pointers

--- dijkstra.c
+++ dijkstra.c
@@ -1,28 +1,26 @@
 //Dijkstra's Pathfinding Algorithm (Matrix Edition)
 
 #include <stdio.h>
+#include <stdlib.h>
 #define INFINITY 2147483647
 #define NEG_INFINITY -2147483647
 #define true 1
 #define false 0
 
-//Define Maximum Sizes
-#define DIJ_MAX_SIZE 9
-
 //Debug Printer for printing all values in distance array
-void debugPrint(int dist[DIJ_MAX_SIZE]){
+void debugPrint(int *dist, int size){
 	printf("Vertex tDistance from Source\n");
-	for(int node = 0; node < DIJ_MAX_SIZE; node++){
+	for(int node = 0; node < size; node++){
 		printf("%d t %d\n", node, dist[node]);
 	}
 }
 
 //Function to get the shortest valid node
-int shortestNode(int dist[DIJ_MAX_SIZE], int checked[DIJ_MAX_SIZE]){
+int shortestNode(int *dist, int *checked, int size){
 	int min = INFINITY;
 	int minIndex;
     //Loop through every node
-	for(int i = 0; i < DIJ_MAX_SIZE; i++){
+	for(int i = 0; i < size; i++){
         //Check if its the new smallest node and it has not already been checked off.
 		if(dist[i] < min && checked[i] == false){
 			min = dist[i];
@@ -34,46 +32,55 @@ int shortestNode(int dist[DIJ_MAX_SIZE], int checked[DIJ_MAX_SIZE]){
 
 //The meat of the the algorithm
 //Takes In:
-//map -> 2d matrix of distances between nodes
-//cMap -> 2d matrix of Connectivity Booleans
+//map -> Pointer of 2d matrix of distances between nodes
+//cMap -> Pointer of 2d matrix of Connectivity Booleans
+//size -> Size of 2d matrixs (Number of nodes)
 //start -> Starting Node
-int dijkstra(int map[DIJ_MAX_SIZE][DIJ_MAX_SIZE], int cMap[DIJ_MAX_SIZE][DIJ_MAX_SIZE], int start){
+int * dijkstra(int *map, int *cMap, int size, int start){
 	//Distance Array -> Contains Values Of Current Smallest Distances
-	int dist[DIJ_MAX_SIZE];
+	int * dist = (int*)malloc(sizeof(int) * size);
     //Set all to infinity
-	for(int i = 0; i < DIJ_MAX_SIZE; i++){dist[i] = INFINITY;}
+	for(int i = 0; i < size; i++){dist[i] = INFINITY;}
 
 	//Checked Array -> Indicates What Arrays Has Already Been Checked
-	int checked[DIJ_MAX_SIZE] = {false};
+	//Using calloc to pre-generate with all zeros
+	int * checked = (int*)calloc(size, sizeof(int));
 
 	//Set the distance of the starting node to 0
 	dist[start] = 0;
 
-	//Loop Through DIJ_MAX_SIZE Times To Go Through All From Nodes
-	for(int cout = 0; cout < DIJ_MAX_SIZE; cout++){
+	//Loop Through size Times To Go Through All From Nodes
+	for(int cout = 0; cout < size; cout++){
 		//Get Smallest Node In Distance Array
-		int from = shortestNode(dist, checked);
+		int from = shortestNode(dist, checked, size);
 		checked[from] = true;
 		//Go Through Each Node, Checking Connections
-		for(int to = 0; to < DIJ_MAX_SIZE; to++){
+		for(int to = 0; to < size; to++){
 			//printf("%d\n", dist[to] > dist[from] + map[from][to]);
             //Checking If:
             //The Nodes Are Connected (using cMap)
             //The New Node Pair Are The Smallest Value
 			//Checked Status Of To Node Is False
-			if(cMap[from][to] == true && dist[to] > dist[from] + map[from][to] && checked[to] == false){
+			if(cMap[(from*size) + to] == true && dist[to] > dist[from] + map[(from*size) + to] && checked[to] == false){
 				//Change Distance To New Shortest Value
-				dist[to] = dist[from] + map[from][to];
+				dist[to] = dist[from] + map[(from*size) + to];
 			}
 		}
 	}
-	//Printing Final Values
-    debugPrint(dist);
+
+	//Free Malloced Memory
+	free(checked);
+
+	return dist;
+
 }
 
-int main(){
 
-	int map[DIJ_MAX_SIZE][DIJ_MAX_SIZE] = {
+// Demo Values
+
+
+int main(){
+	int map[9][9] = {
 	{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
 	{4, 0, 8, 0, 0, 0, 0, 11, 0}, 
 	{0, 8, 0, 7, 0, 4, 0, 0, 2}, 
@@ -84,7 +91,7 @@ int main(){
 	{8, 11, 0, 0, 0, 0, 1, 0, 7}, 
 	{0, 0, 2, 0, 0, 0, 6, 7, 0}};
 
-	int cMap[DIJ_MAX_SIZE][DIJ_MAX_SIZE] = {
+	int cMap[9][9] = {
 	{0, 1, 0, 0, 0, 0, 0, 1, 0}, 
 	{1, 0, 1, 0, 0, 0, 0, 1, 0}, 
 	{0, 1, 0, 1, 0, 1, 0, 0, 1}, 
@@ -95,5 +102,12 @@ int main(){
 	{1, 1, 0, 0, 0, 0, 1, 0, 1}, 
 	{0, 0, 1, 0, 0, 0, 1, 1, 0}};
 
-	dijkstra(map, cMap, 0);
-}
+	int * result = dijkstra(&map[0][0], &cMap[0][0], 9, 0);
+	
+	//Printing Final Values
+    debugPrint(result, 9);
+
+	//Free result
+	free(result);
+	
+}	
